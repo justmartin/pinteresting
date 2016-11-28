ff51a91c6e7d6e512af20d68002efb288e603723
justmartin <justin.lamar.martin@gmail.com>
2016-11-28 15:26:14 -0800

    Added name to user and validations for pin/user creation.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ApplicationRecord
   
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates :description, presence: true
+  validates :image, presence: true
 end
