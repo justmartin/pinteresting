52d631897c9e867a12a5f95e01b7ef7054981133
justmartin <justin.lamar.martin@gmail.com>
2016-11-28 20:30:30 -0800

    Adjusted margin and width on pins.

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,7 @@
 class Pin < ApplicationRecord
   belongs_to :user
   
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { xx_large: "900x900>", x_large: "700x700>", large: "500x500>", medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   validates :description, presence: true
